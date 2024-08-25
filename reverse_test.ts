269ae0caec59eeb8343f68ace2274676e7b4be5c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-25 16:25:22 +0100

    fix(reverse): add docs/modules, fix test

--- reverse_test.ts
+++ reverse_test.ts
@@ -5,14 +5,14 @@ import { reverse } from "./reverse.ts";
 
 await test("reverse with non empty iterable", () => {
   const result = reverse([1, 2, 3, 4, 5] as const);
-  const expected = [5, 4, 3, 2, 1] as const;
-  assertEquals(result, expected);
-  assertType<IsExact<typeof result, Iterable<[5, 4, 3, 2, 1]>>>(true);
+  const expected = [5, 4, 3, 2, 1];
+  assertEquals(Array.from(result), expected);
+  assertType<IsExact<typeof result, Iterable<1 | 2 | 3 | 4 | 5>>>(true);
 });
 
 await test("reverse with empty iterable", () => {
   const result = reverse([] as number[]);
-  const expected = [];
-  assertEquals(result, expected);
-  assertType<IsExact<typeof result, number[]>>(true);
+  const expected: number[] = [];
+  assertEquals(Array.from(result), expected);
+  assertType<IsExact<typeof result, Iterable<number>>>(true);
 });
